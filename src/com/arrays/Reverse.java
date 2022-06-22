486a6142db9a02c0f0c04c27caa2e49345b82ffc
juhisindhia <juhisindhia@outlook.com>
2022-06-22 23:56:47 +0530

    Reverse Array

---
+++ src/com/arrays/Reverse.java
@@ -0,0 +1,35 @@
+package com.arrays;
+
+public class Reverse {
+	public static void main(String[] args) {
+		int[] arr = new int[5];
+		arr[0]=10;
+		arr[1]=20;
+		arr[2]=30;
+		arr[3]=40;
+		arr[4]=50;	
+
+		System.out.print("Befor swapping...  ");
+		for(int i : arr){
+			System.out.print(" "+i);
+		}
+		System.out.println();
+		
+	int temp = 100;
+	temp = arr[4];
+	arr[4]= arr[0];
+	arr[0] = temp;
+
+	temp = arr[1];
+	arr[1]= arr[3];
+	arr[3] = temp;
+
+	System.out.print("After swapping...  ");
+	for(int i : arr){
+		System.out.print(" "+i);
+	}
+			
+	}
+	
+
+}
